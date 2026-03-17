bc760b570af99b321ce9960a4c11d64dd76a3d19
TanBinh <buitanbinh2005@gmail.com>
2026-03-17 16:17:08 +0700

    deploy

--- be/src/server.js
+++ be/src/server.js
@@ -3,18 +3,32 @@ import todoRoutes from './routes/todoRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 const PORT = process.env.PORT||5001;
+const __dirname = path.resolve();
+
 const app = express();
 
 
 
 // Middleware
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:5173'}));
+if(process.env.NODE_ENV !== 'production') {
+    app.use(cors({origin: 'http://localhost:5173'}));
+}
 
 app.use('/api/todos', todoRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../fe/dist")));
+
+app.get('*',(req,res) =>{
+    res.sendFile(path.join(__dirname, "../fe/dist/index.html"));
+})
+}
+
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
